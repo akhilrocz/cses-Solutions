c9ed6ad9b9743d1b1b19727520288818857361d0
Akhil roczz <b.akhil667@gmail.com>
2024-12-19 12:37:40 +0530

    Permutations

--- Permutations.java
+++ Permutations.java
@@ -1,2 +1,26 @@
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.*;
 public class Permutations {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner =new Scanner(System.in);
+        long n=scanner.nextLong();
+        if(n==2 || n==3){
+            System.out.println("NO SOLUTION");
+            return;
+        }
+        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+        for(int i=2;i<=n;i+=2){
+            out.write(i+" ");
+        }
+        for(int i=1;i<=n;i+=2){
+            out.write(i+" ");
+        }
+        out.flush();
+    }
+
+
+
 }
